3338f81161dc9bdae89eb98c12b5cae872117f5c
William Abodunrin <70331030+jesulonimii@users.noreply.github.com>
2021-02-16 02:19:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -82,11 +82,6 @@ if(!isNaN(input)) {
 
 }
 
-let input = document.getElementById('input').value;
-let result = document.getElementById('result');
-let hide = document.getElementById('closeBtn');
-
-
 
 function closeResult() {
   
